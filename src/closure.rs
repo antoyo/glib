2fa1d7974b27832998383c5e11c164d64f6e70b6
Antoni Boucher <bouanto@zoho.com>
2017-05-24 16:50:09 -0400

    Update closure

--- src/closure.rs
+++ src/closure.rs
@@ -2,11 +2,13 @@
 
 use std::mem;
 use std::ptr;
+use std::slice;
 
 use libc::{c_uint, c_void};
 
 use gobject_ffi;
-use translate::{FromGlibPtrFull, FromGlibPtrNone, Stash, StashMut, ToGlibPtr, ToGlibPtrMut, Uninitialized};
+use translate::{Stash, StashMut, ToGlibPtr, ToGlibPtrMut, Uninitialized, mut_override};
+use types::Type;
 use Value;
 use ToValue;
 
@@ -17,36 +19,28 @@ pub struct Closure {
 impl Closure {
     pub fn new<F: Fn(&[Value]) -> Option<Value> + 'static>(callback: F) -> Self {
         unsafe extern "C" fn marshal<F: Fn(&[Value]) -> Option<Value> + 'static>(_closure: *mut gobject_ffi::GClosure,
-            return_value: *mut gobject_ffi::GValue, n_param_values: c_uint, mut param_values: *mut gobject_ffi::GValue,
+            return_value: *mut gobject_ffi::GValue, n_param_values: c_uint, param_values: *mut gobject_ffi::GValue,
             _invocation_hint: *mut c_void, marshal_data: *mut c_void)
         {
-            let mut values = vec![];
-            for _ in 0 .. n_param_values {
-                let value: Value = FromGlibPtrNone::from_glib_none(param_values);
-                values.push(value);
-                param_values = param_values.offset(1);
+            let values = slice::from_raw_parts(param_values as *const _, n_param_values as usize);
+            let callback: Box<F> = Box::from_raw(marshal_data as *mut _);
+            let result = callback(values);
+            if !return_value.is_null() {
+                match result {
+                    Some(result) => *return_value = result.into_raw(),
+                    None => {
+                        let result = Value::uninitialized();
+                        *return_value = result.into_raw();
+                    },
+                }
             }
-            let callback: &Box<F> = mem::transmute(marshal_data as *mut _);
-            let result = callback(&values);
-            match result {
-                Some(result) => *return_value = result.into_raw(),
-                None => {
-                    let result = Value::uninitialized();
-                    let mut gvalue = result.into_raw();
-                    gvalue.g_type = gobject_ffi::G_TYPE_NONE;
-                    *return_value = gvalue;
-                },
-            }
-            // TODO: remove this.
-            for value in values {
-                mem::forget(value);
-            }
+            mem::forget(callback);
         }
 
         unsafe extern "C" fn finalize<F: Fn(&[Value]) -> Option<Value> + 'static>(notify_data: *mut c_void,
             _closure: *mut gobject_ffi::GClosure)
         {
-            let _callback: Box<Box<F>> = Box::from_raw(notify_data as *mut _);
+            let _callback: Box<F> = Box::from_raw(notify_data as *mut _);
             // callback is dropped here.
         }
 
@@ -54,34 +48,34 @@ impl Closure {
             let size = 4 + 4 + 3 * mem::size_of::<*mut c_void>() as u32;
             let closure = gobject_ffi::g_closure_new_simple(size, ptr::null_mut());
             assert_ne!(closure, ptr::null_mut());
-            let callback = Box::new(Box::new(callback));
-            let ptr: *mut Box<F> = Box::into_raw(callback);
+            let callback = Box::new(callback);
+            let ptr: *mut F = Box::into_raw(callback);
             let ptr: *mut c_void = ptr as *mut _;
             gobject_ffi::g_closure_set_meta_marshal(closure, ptr, Some(marshal::<F>));
             gobject_ffi::g_closure_add_finalize_notifier(closure, ptr, Some(finalize::<F>));
+            gobject_ffi::g_closure_ref(closure);
+            gobject_ffi::g_closure_sink(closure);
             Closure {
                 closure: closure,
             }
         }
     }
 
-    pub fn invoke(&mut self, values: &[&ToValue]) -> Option<Value> {
-        let mut result: gobject_ffi::GValue = unsafe { mem::zeroed() };
-        let values: Vec<_> = values.iter()
+    pub fn invoke(&self, values: &[&ToValue]) -> Option<Value> {
+        let result = unsafe { Value::uninitialized() };
+        let mut values: Vec<_> = values.iter()
             .map(|v| v.to_value())
             .collect();
-        let mut gvalues: Vec<gobject_ffi::GValue> = values.iter()
-            .map(|v| unsafe { ptr::read(v.to_glib_none().0) })
-            .collect();
+        let gvalues = values.as_mut_ptr() as *mut _;
         unsafe {
-            gobject_ffi::g_closure_invoke(self.to_glib_none_mut().0, &mut result, gvalues.len() as u32,
-                gvalues.as_mut_ptr(), ptr::null_mut());
+            gobject_ffi::g_closure_invoke(self.to_glib_none().0 as *mut _, mut_override(result.to_glib_none().0),
+                values.len() as u32, gvalues, ptr::null_mut());
         }
-        if result.g_type == gobject_ffi::G_TYPE_NONE {
+        if result.type_() == Type::Invalid {
             None
         }
         else {
-            Some(unsafe { Value::from_glib_full(&mut result as *mut _) })
+            Some(result)
         }
     }
 }
@@ -138,7 +132,7 @@ mod tests {
         let call_count = Rc::new(AtomicUsize::new(0));
 
         let count = call_count.clone();
-        let mut closure = Closure::new(move |values| {
+        let closure = Closure::new(move |values| {
             count.fetch_add(1, Ordering::Relaxed);
             assert_eq!(values.len(), 2);
             let string: Option<String> = values[0].get();
@@ -155,7 +149,7 @@ mod tests {
         assert!(result.is_none());
         assert_eq!(call_count.load(Ordering::Relaxed), 2);
 
-        let mut closure = Closure::new(closure_fn);
+        let closure = Closure::new(closure_fn);
         let result = closure.invoke(&[&"test".to_string(), &42]);
         let int: Option<i32> = result.and_then(|result| result.get());
         assert_eq!(int, Some(24));
